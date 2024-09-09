cf59b2088322657efb78fcc4dd495dd468fe331d
Pavan0896 <pavankumar170896@gmail.com>
2024-09-09 21:40:49 +0530

    Added routes

--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Task = require('../models/task');
-const TaskHistory = require('../models/taskHistory');
+const Task = require("../models/task");
+const TaskHistory = require("../models/taskHistory");
 
-router.get('/tasks', async (req, res) => {
+router.get("/tasks", async (req, res) => {
   try {
     const tasks = await Task.find();
     res.json(tasks);
@@ -12,7 +12,7 @@ router.get('/tasks', async (req, res) => {
   }
 });
 
-router.get('/task-history', async (req, res) => {
+router.get("/task-history", async (req, res) => {
   try {
     const histories = await TaskHistory.find();
     res.json(histories);
@@ -21,7 +21,7 @@ router.get('/task-history', async (req, res) => {
   }
 });
 
-router.post('/tasks', async (req, res) => {
+router.post("/tasks", async (req, res) => {
   const task = new Task(req.body);
   try {
     const newTask = await task.save();
@@ -31,13 +31,39 @@ router.post('/tasks', async (req, res) => {
   }
 });
 
-router.post('/task-history', async (req, res) => {
+router.post("/task-history", async (req, res) => {
   const history = new TaskHistory(req.body);
   try {
     const newHistory = await history.save();
     res.status(201).json(newHistory);
   } catch (error) {
     res.status(400).json({ message: error.message });
+  }
+});
+
+router.put("/tasks/:id", async (req, res) => {
+  try {
+    const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+    if (!updatedTask) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.json(updatedTask);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.delete("/tasks/:id", async (req, res) => {
+  try {
+    const task = await Task.findByIdAndDelete(req.params.id);
+    if (!task) {
+      return res.status(404).json({ message: "Task not found" });
+    }
+    res.json({ message: "Task deleted" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
