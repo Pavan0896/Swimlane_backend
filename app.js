8f370df0ef4b3be7824ed7de30db081dfc102c50
Pavan0896 <pavankumar170896@gmail.com>
2024-09-09 21:32:16 +0530

    Cors added

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const taskRoutes = require('./routes/taskRoutes');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const taskRoutes = require("./routes/taskRoutes");
+require("dotenv").config();
+const cors = require("cors");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => console.log('MongoDB connected'))
-  .catch(err => console.log(err));
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
-app.use('/api', taskRoutes);
+app.use("/api", taskRoutes);
 
-app.get('/health', (req, res) => {
-  res.status(200).json({ status: 'UP' });
+app.get("/health", (req, res) => {
+  res.status(200).json({ status: "UP" });
 });
 
 const PORT = process.env.PORT;
